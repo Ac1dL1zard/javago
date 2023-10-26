d08d4294cebbc5adbdf775f9a57076b1c70cc6dc
oanamocanu997 <oanamocanu997@gmail.com>
2023-10-26 17:36:07 +0200

    Javago V1

--- src/main/java/com/generation/javago/model/entity/Room.java
+++ src/main/java/com/generation/javago/model/entity/Room.java
@@ -1,6 +1,56 @@
 package com.generation.javago.model.entity;
 
-public class Room
+import java.util.ArrayList;
+import java.util.List;
+
+import com.generation.javago.model.library.BaseEntity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Entity
+public class Room extends BaseEntity
 {
 
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	
+	private String name;
+	private String imgUrl;
+	private Integer capacity;
+	private Double basePrice;
+	private String notes;
+	
+	@OneToMany(mappedBy="room", fetch = FetchType.EAGER)
+	private List<RoomBooking> bookings;
+	
+	@Override
+	public List<String> getErrors() 
+	{
+		List<String> errors = new ArrayList<>();
+	    
+		if (name==null)
+		{
+			errors.add("Missing or invalid value for field 'name");
+		}
+		if (capacity<=0)
+			errors.add("Missing or invalid value for field 'capacity");
+		if (basePrice<=0)
+			errors.add("Missing or invalid value for field 'basePrice");
+	    
+	    return errors;
+	}
+	
+	
 }
